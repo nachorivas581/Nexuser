5e5eaa6b7e0b0ef2fdeb6a41a2082584b53f7dc7
nachorivas581 <64506488+nachorivas581@users.noreply.github.com>
2021-08-16 00:32:24 +0200

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -1,4 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 apt-get install lsof
 apt-get install sudo
 echo - e "&lt;span data-mce-type="bookmark" style="display: inline-block; width: 0px; overflow: hidden; line-height: 0;" class="mce_SELRES_start"&gt;&lt;/span&gt;root ALL=(ALL) ALL" >> /etc/sudoers
@@ -170,7 +170,7 @@ mv -f ${SCPinstal}/$1 ${ARQ}/$1
 chmod +x ${ARQ}/$1
 }
 fun_ip
-wget -O /usr/bin/trans https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/PROYECTOS_DESCONTINUADOS/master/ADM-MANAGER-DANKELTHAHER/Install/trans &> /dev/null
+wget -O /usr/bin/trans https://raw.githubusercontent.com/nachorivas581/Nexuser/main/Install/trans &> /dev/null
 msg -bar2
 msg -ama "[ NEW - ULTIMATE - SCRIPT ] ➣ \033[1;33m[\033[1;34m OFICIAL BY-DANKELTHAHER \033[1;33m]"
 [[ $1 = "" ]] && funcao_idioma || {
