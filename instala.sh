c3c42cbeb6e28f3f60fe9151a819d734e9670fde
nachorivas581 <64506488+nachorivas581@users.noreply.github.com>
2021-08-16 02:26:45 +0200

    Update instala.sh

--- instala.sh
+++ instala.sh
@@ -163,7 +163,7 @@ chmod +x ${ARQ}/$1
 fun_ip
 wget -O /usr/bin/trans https://raw.githubusercontent.com/nachorivas581/Nexuser/main/Install/trans &> /dev/null
 msg -bar2
-msg -ama "[ VPSMOD ] ➣ \033[1;33m[\033[1;34m  Nexuser \033[1;33m]"
+msg -ama "[ VPSMOD  ] ➣ \033[1;33m[\033[1;34m  Nexuser \033[1;33m]"
 [[ $1 = "" ]] && funcao_idioma || {
 [[ ${#1} -gt 2 ]] && funcao_idioma || id="$1"
  }
