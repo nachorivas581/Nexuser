7dcd7808997700eb2a72c1a055fd50a61d2bb28f
nachorivas581 <64506488+nachorivas581@users.noreply.github.com>
2021-08-29 00:15:20 +0200

    Update Nexuser.sh

--- Nexuser.sh
+++ Nexuser.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-apt update -y ; apt upgrade -y ;wget https://raw.githubusercontent.com/nachorivas581/Nexuser/main/instala.sh; chmod 777 instala.sh* && ./instala.sh* && 
-wget https://raw.githubusercontent.com/nachorivas581/Nexuser/main/request/Actualizacion ;chmod +x Actualizacion ;echo -e "\033[7;32m Instalando Actualizacion\e[0m " ;sleep 2 ;echo -e "\033[1;32mEspere un momento...\e[0m" ;bash Actualizacion &> /dev/null ; echo -e "\033[7;32m Actualizado Exitosamente\e[0m " ;sleep 2 ; echo -e "\033[1;32mEjecuta menu/adm para usar el script\e[0m"
-
+apt update -y ; apt upgrade -y ;wget https://raw.githubusercontent.com/nachorivas581/Nexuser/main/instala.sh; chmod 777 instala.sh* && ./instala.sh*
